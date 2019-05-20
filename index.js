c58a29316e008745544dc3c37d6c211ac65cc708
mpedroc90 <mpedroc90@gmail.com>
2019-05-20 08:13:50 -0400

    Load data from api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.env.API_URL =  process.env.API_URL ||  "http://localhost:3500"
+process.env.API_URL =  process.env.API_URL ||  "http://netzone.cl/bntf/api.users.prueba/skeleton/api"
 process.env.API_TOKEN =  process.env.API_TOKEN || '%ca7b=E]bV?t_M8C(Q]qU{qzQTPJOX/%AoKVv3S`Z`"Uxh]uwBfnooPJ%DW9)]m'
 
 var app = require("./app")
--- models/datasources.js
+++ models/datasources.js
@@ -2,9 +2,9 @@ var axios = require("axios");
 var users = [];
 
 if (users.length === 0) {
-  let url = process.env.API_URL; 
+  let url = process.env.API_URL;
   const headers = {
-    token: process.env.API_TOKEN
+    token: process.env.API_TOKEN,
   };
 
   axios
@@ -19,21 +19,21 @@ if (users.length === 0) {
           users[index].skills = skills
             .filter(t => t.hasOwnProperty(u.id))
             .map(t => t[u.id])[0];
-          /*if (u.format_image === 1) {
-          headers.iduser = u.id;
-             const r = axios
-            .get(`${url}/image_perfil?id=${Math.random()}`, {
-              headers,
+          if (u.format_image === 1) {
+            headers.iduser = u.id;
+            const r = axios
+              .get(`${url}/image_perfil?id=${Math.random()}`, {
+                headers,
 
-              responseType: "arraybuffer",
-            })
-            .then(img_res => {
-              users[index].url_image =
-                "data:image/jpeg;base64," +
-                new Buffer.from(img_res.data, "binary").toString("base64");
-            })
-            .catch(e => console.log(e));
-        }*/
+                responseType: "arraybuffer",
+              })
+              .then(img_res => {
+                users[index].url_image =
+                  "data:image/jpeg;base64," +
+                  new Buffer.from(img_res.data, "binary").toString("base64");
+              })
+              .catch(e => console.log(e));
+          }
         });
       });
     });
